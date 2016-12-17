5c99444f7b5c8fc7eff58327d27ba80d2ef00a78
Diego Mendoza <dmendoza@inflexionsoftware.com>
2016-12-16 22:50:04 -0600

    Setup project chalet survey base on angular-boleriplate by Diego Men

---
+++ js/app/home/home.js
@@ -0,0 +1,20 @@
+( function() {
+	'use strict';
+
+	angular.module('chaiApp.home').controller('HomeCtrl', HomeCtrl);
+
+	HomeCtrl.$inject = ['$routeParams'];
+
+	/**@ngInject*/
+	function HomeCtrl($routeParams){
+		var homeCtrl = this;
+
+		//Initialize controller
+		activate();
+
+		function activate(){
+			console.log('Activated HomeCtrl');	
+			console.log('All is ok!');
+		}
+	}
+})();
