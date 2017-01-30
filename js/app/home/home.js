a177e52c563a446300e29a415375af2679d61462
Diego Mendoza <dmendoza@inflexionsoftware.com>
2017-01-29 22:49:32 -0600

    Agregue la funcionalidad a los botones de la encuesta para guardar la respuesta de cada pregunta

--- js/app/home/home.js
+++ js/app/home/home.js
@@ -3,11 +3,16 @@
 
 	angular.module('surveyApp.home').controller('HomeCtrl', HomeCtrl);
 
-	HomeCtrl.$inject = ['$routeParams'];
+	HomeCtrl.$inject = ['$routeParams','$location'];
 
 	/**@ngInject*/
-	function HomeCtrl($routeParams){
+	function HomeCtrl($routeParams, $location){
 		var homeCtrl = this;
+		//Events
+		this.saveSurvey = saveSurvey;
+
+		//Variables
+		this.fstAnswer = '';
 
 		//Initialize controller
 		activate();
@@ -15,6 +20,14 @@
 		function activate(){
 			console.log('Activated HomeCtrl');	
 			console.log('All is fine!');
+		}
+
+		function saveSurvey(){
+			console.log(this.fstAnswer);
+			console.log(this.secAnswer);
+			console.log(this.thirdAnswer);
+			console.log(this.fourthAnswer);
+			$location.path('/sent-message');
 		}
 	}
 })();
