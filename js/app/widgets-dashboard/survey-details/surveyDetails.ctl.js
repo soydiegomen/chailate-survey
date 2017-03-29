26ea47c9fcdee303a9641e227083a72650e4bad3
Diego Mendoza <dmendoza@inflexionsoftware.com>
2017-03-28 22:41:19 -0600

    Armado del model que utilizará el widget survey-details para llenar la información de la pregunta y su respectiva respuesta

--- js/app/widgets-dashboard/survey-details/surveyDetails.ctl.js
+++ js/app/widgets-dashboard/survey-details/surveyDetails.ctl.js
@@ -13,27 +13,51 @@
 
         function activate(){
         	console.log('Activated SurveyDetailsCtrl');	
-        	getLastAnswer().then(getChailateSurvey).then(function(data) {
-        		console.log('results');
-        		console.log(ctrl.answer);
-        		console.log(ctrl.survey);
+        	getChailateSurvey().then(getLastAnswer).then(function(result) {
+        		ctrl.questions = fillAnswersInQuestions(result);
+
+        		var answer = result.answer[0];
+        		ctrl.answerDate = new Date(answer.creationDate);
+        		ctrl.key = (answer.key && answer.key.length > 0) ?
+        			answer.key : 'vacía';
         	});
         }
 
-        function getLastAnswer(){
+        function fillAnswersInQuestions(data){
+        	var questions = data.survey.questions;
+        	var answers = data.answer[0].details;
+        	
+        	//Iterate questions for fill the answer
+        	questions.forEach(function(question) {
+        		answers.forEach(function(ans) {
+        			//find the answer to the current question
+        			if(question._id === ans.questionId){
+        				//assing the answer value
+        				question.answer = ans.value;
+        			}
+        		});
+        	});
+        	console.log(data.answer[0]);
+        	return questions;
+        }
+
+        function getLastAnswer(survey){
         	var date = new Date();
 			var isoDate = date.toISOString();
 			return dataservice.getLastAnswer(isoDate)
 				.then(function(data) {
-					ctrl.answer = data;
-					return data;
+					var result = { 
+						survey : survey, 
+						answer : data
+					};
+					
+					return result;
 				});
 		}
 
 		function getChailateSurvey(){
 			return dataservice.getChailateSurvey()
 				.then(function(data) {
-					ctrl.survey = data;
 					return data;
 				});
 		}
